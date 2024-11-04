9abfe10afb2d71ed36d2a55f95acd9b1cdf4244c
rahul <rahul.0sharma0.007@gmail.com>
2024-11-04 16:05:21 +0530

    my commit

---
+++ labrahul/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from react;
+function Home() {
+return (
+<div>
+
+    <h2>Welcome to the Home Page</h2>
+    <p>This is a protected pahge accessible after login</p>
+</div>
+);
+}
+export default Home;
